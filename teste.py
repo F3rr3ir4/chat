9ab7970ffb5858434fdba395410f0f26d00e3402
F3rr3ir4 <leandroferreira757@msn.com>
2019-11-01 19:50:24 -0300

    Instalado virtualeven

--- teste.py
+++ teste.py
@@ -1,15 +1,40 @@
+#importar as libs
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import keys
+
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
 
+#Instanciar Chatbot
 
 chatbot = ChatBot("Ananda")
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.portuguese")
+trainerer = ListTrainer(chatbot)
 
-while True:
+#armazenar dirretorio principal em variavel
+dir_pach = os.getcwd()
+
+#iniciar aplicação
+driver = webdriver.Chrome(dir_pach+'chromedrive.exe')
+driver.get('https://web.whatsapp.com/')
+driver.implicitly_wait(15)
+
+#Funções básicas de comunicação
+def pegaConversa():
   try:
-      bot_input = chatbot.get_response(input())
-      print(bot_input)
+    ...
   except:
-    print("Algo deu errado... Tchau!")
+    ...
+
+def enviaMensagem(mensagem):
+  ...
+
+def treinar(mensagem):
+
+  ...
